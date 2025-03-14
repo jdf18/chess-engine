9ad1f13401d6635e937b1be8b5ca34ebc66b4454
Michael Simm <179273005+mickyj0101@users.noreply.github.com>
2025-03-14 17:43:30 +0000

    Finish implementing pawn movement

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -42,10 +42,10 @@ uint64_t valid_knight_moves(uint64_t square, uint64_t pieces_col) {
 	allsquares |= square >> 6;
 	allsquares |= square >> 15;
 	allsquares |= square >> 17;
-	allsquares |= square << 6;
-	allsquares |= square << 10;
-	allsquares |= square << 15;
-	allsquares |= square << 17;
+	allsquares |= (square & (UINT64_MAX >> 6)) << 6;
+	allsquares |= (square & (UINT64_MAX >> 10)) << 10;
+	allsquares |= (square & (UINT64_MAX >> 15)) << 15;
+	allsquares |= (square & (UINT64_MAX >> 17)) << 17;
 	return allsquares & ~pieces_col;
 }
 
@@ -53,13 +53,13 @@ uint64_t valid_knight_moves(uint64_t square, uint64_t pieces_col) {
 // which is the position of all pieces, and 'pieces_col', the position of
 // all pieces of the same colour as the pawn.
 // This does factor in the pawn making a double move if it's on the second rank.
-// It requires a boolean for the colour. 1 for white and 0 for black.
-uint64_t valid_pawn_moves(uint64_t square, uint64_t pieces, uint64_t pieces_col, bool colour) {
+// It requires a boolean for the colour. True for white and False for black.
+uint64_t valid_pawn_moves(uint64_t square, uint64_t pieces, uint64_t pieces_col, bool white) {
 	// In the case that the pawn is white, it moves two squares if on the second rank.
 	// The direction it moves is also different.
 	// I don't think this can be written without the branch on colour, but the second branch (checking if it's on the second rank) could maybe
 	// be removed with some bit manipulation wizardry.
-	if (colour) {
+	if (white) {
 		uint64_t allsquares = 0;
 		// Check if the pawn is on the second rank
 		if ((square & (255 << 8)) > 0) {
@@ -71,16 +71,39 @@ uint64_t valid_pawn_moves(uint64_t square, uint64_t pieces, uint64_t pieces_col,
 			// This is potentially incorrect, but I think it should work.
 			// We XOR the spaces occupied by other pieces with the space the pawn is on, so it doesn't interfere.
 			// We then take these spaces and move them all up one square (by left shifting by 8).
-			// We can then bitwise and allsquares with NOT of this.
+			// We can then bitwise AND allsquares with NOT of this.
 			// This should take care of the case where there is a piece directly in front of the pawn, but not the square after that.
-			allsquares &= ~((pieces ^ square) << 8);
+			allsquares &= ~(((pieces ^ square) & (UINT64_MAX >> 8)) << 8);
 		}
 		else {
-			allsquares |= square << 8;
+			allsquares |= (square & (UINT64_MAX >> 8)) << 8;
 			allsquares &= pieces;
 		}
 		allsquares |= square << 7;
 		allsquares |= square << 9;
-		allsquares &= ~pieces;
+		allsquares &= ~pieces_col;
+		return allsquares;
+	}
+	else {
+		uint64_t allsquares = 0;
+		// Check if the pawn is on the seventh rank
+		if ((square & (255 << 48)) > 0) {
+			// Add the two squares the pawn could move to in theory
+			allsquares |= square >> 8;
+			allsquares |= square >> 16;
+			// Remove squares occupied by pieces
+			allsquares &= ~pieces;
+			// This should take care of the case where there is a piece directly in front of the pawn, but not the square after that.
+			// Same reasoning as for the white case.
+			allsquares &= ~((pieces ^ square) >> 8);
+		}
+		else {
+			allsquares |= square >> 8;
+			allsquares &= pieces;
+		}
+		allsquares |= square >> 7;
+		allsquares |= square >> 9;
+		allsquares &= ~pieces_col;
+		return allsquares;
 	}
 }
