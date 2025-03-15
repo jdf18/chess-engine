ad9f25e98e3878c8075fb7e5da621528e27f0fbd
Jacob <97624626+jdf18@users.noreply.github.com>
2025-03-15 22:13:45 +0000

    Added enums for player colour and piece types, created cmake script that should hopefully work on windows

---
+++ src/engine/pieces.h
@@ -0,0 +1,13 @@
+typedef enum {
+    COL_BLACK,
+    COL_WHITE
+} Colour;
+
+typedef enum {
+    PIECE_KING,
+    PIECE_QUEEN,
+    PIECE_ROOK,
+    PIECE_KNIGHT,
+    PIECE_BISHOP,
+    PIECE_PAWN
+} Pieces;
