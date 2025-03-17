c0c66cdac5b4e9d5ad077890607c7ddd127a9b8c
Michael Simm <179273005+mickyj0101@users.noreply.github.com>
2025-03-17 16:57:21 +0000

    test some stuff

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,6 +1,37 @@
 #include "boardstate.h"
 
 #include <iostream>
+#include <bitset>
+
+BitBoard reverse_board(BitBoard to_reverse) {
+    BitBoard rev = 0;
+
+    for (int i = 0; i < 64; i++) {
+        rev <<= 1;
+
+        if ((to_reverse & 1) == 1) {
+            rev ^= 1;
+        }
+
+        to_reverse >>= 1;
+    }
+    return rev;
+}
+
+static void out_bitboard(BitBoard to_out) {
+    std::cout << std::bitset<64>(to_out).to_string() << std::endl;
+    to_out = reverse_board(to_out);
+    std::cout << std::bitset<64>(to_out).to_string() << std::endl;
+    std::cout << std::endl;
+    std::cout << std::bitset<8>(to_out & 0xFF).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF00) >> 8).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF0000) >> 16).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF000000) >> 24).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF00000000) >> 32).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF0000000000) >> 40).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF000000000000) >> 48).to_string() << std::endl;
+    std::cout << std::bitset<8>((to_out & 0xFF00000000000000) >> 56).to_string() << std::endl;
+}
 
 int main() {
     BoardState initial_state;
@@ -10,8 +41,12 @@ int main() {
     BitBoard possible_moves;
     possible_moves = initial_state.pseudo_legal_knights_moves(COL_WHITE);
 
-    std::cout << (uint64_t)initial_state.pieces_knights << '\n';
-    std::cout << (uint64_t)possible_moves << '\n';
+    out_bitboard(initial_state.pieces_knights);
+
+    std::cout << std::endl;
+
+    out_bitboard(possible_moves);
+
 
     return 0;
 }
