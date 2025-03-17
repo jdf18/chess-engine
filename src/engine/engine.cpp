5c0412ba243a36e5b7d26bd72e312bd061d96392
Jacob <97624626+jdf18@users.noreply.github.com>
2025-03-17 00:06:40 +0000

    Writing some test code so can see the result of the pseudo_legal_knight_moves method

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,6 +1,17 @@
 #include "boardstate.h"
 
+#include <iostream>
+
 int main() {
     BoardState initial_state;
+    initial_state.pieces_knights = 0x0001000000001000;
+    initial_state.pieces_white = initial_state.pieces_knights;
+
+    BitBoard possible_moves;
+    possible_moves = initial_state.pseudo_legal_knights_moves(COL_WHITE);
+
+    std::cout << (uint64_t)initial_state.pieces_knights << '\n';
+    std::cout << (uint64_t)possible_moves << '\n';
+
     return 0;
 }
