e97d9825aea0edddbfb703fe8ca916bbc2f62686
Jacob <97624626+jdf18@users.noreply.github.com>
2025-04-10 18:50:25 +0100

    Switching to use the new setup_default method in main() instead of magic numbers

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -5,16 +5,7 @@
 
 int main() {
     BoardState initial_state;
-
-    initial_state.pieces_white = FIRST_RANK | SECOND_RANK;
-    initial_state.pieces_black = EIGHTH_RANK | SEVENTH_RANK;
-
-    initial_state.pieces_kings = (FIRST_RANK | EIGHTH_RANK) & FIFTH_FILE;
-    initial_state.pieces_queens = (FIRST_RANK | EIGHTH_RANK) & FOURTH_FILE;
-    initial_state.pieces_rooks = (FIRST_RANK | EIGHTH_RANK) & (FIRST_FILE | EIGHTH_FILE);
-    initial_state.pieces_bishops = (FIRST_RANK | EIGHTH_RANK) & (THIRD_FILE | SIXTH_FILE);
-    initial_state.pieces_knights = (FIRST_RANK | EIGHTH_RANK) & (SECOND_FILE | SEVENTH_FILE);
-    initial_state.pieces_pawns = SECOND_RANK | SEVENTH_RANK;
+    initial_state.setup_default();
 
     initial_state.print();
     std::cout << std::endl << initial_state.get_fen();
