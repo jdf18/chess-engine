47befe9e8aa8af74c0158dfae2f783686ba1e946
Jacob Flint <97624626+jdf18@users.noreply.github.com>
2025-04-11 16:06:18 +0100

    Revert "Get file attacks mickyj0101"

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -6,36 +6,9 @@
 int main() {
     BoardState initial_state;
     initial_state.setup_default();
-  
+
     initial_state.print();
     std::cout << std::endl << initial_state.get_fen();
-
-    BitBoard possible_moves;
-    possible_moves = initial_state.pseudo_legal_knights_moves(COL_WHITE);
-
-    initial_state.pieces_knights.out();
-
-    std::cout << std::endl;
-
-    possible_moves.out();
-
-    std::cout << std::endl;
-
-    BitBoard rook_pos = 0x0000000000000008;
-    rook_pos.out();
-
-    initial_state.pieces_white |= rook_pos;
-
-    BitBoard rook_moves = initial_state.pseudo_legal_rook_moves(COL_WHITE, rook_pos);
-    BitBoard bishop_moves = initial_state.pseudo_legal_bishop_moves(COL_WHITE, rook_pos);
-
-    std::cout << std::endl;
-
-    rook_moves.out();
-
-    std::cout << std::endl;
-
-    bishop_moves.out();
 
     return 0;
 }
