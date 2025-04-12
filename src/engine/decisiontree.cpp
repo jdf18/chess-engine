2cf7836cd526e166decfac9c364e988b90b29c16
Jacob <97624626+jdf18@users.noreply.github.com>
2025-04-12 17:16:02 +0100

    Generate moves methods on the DecisionTree

--- src/engine/decisiontree.cpp
+++ src/engine/decisiontree.cpp
@@ -15,20 +15,47 @@ void DecisionTreeNode::add_child(NodeData child_data) {
 }
 
 void DecisionTreeNode::generate_moves() {
-    BoardState current_board = data.board_state;
-    // for piece in board.
-    //     for possible move in piece
-    //         new_data.previous_move = ...;
-    //         new_data.board_state = current_board;
-    //         update the board state
-    //         add_child(new_data)
-    for (PieceInstance piece_instance : current_board.pieces) {
-        for (Move possible_move : piece_instance.generateMoves(current_board)) {
-            NodeData new_data{current_board, possible_move};
+    const BoardState* current_board = &data.board_state;
+
+    for (uint8_t i = 0; i < 31; i++) {
+        PieceInstance* piece_instance = &data.board_state.pieces[i];
+        piece_instance->position.print();
+        for (const Move& possible_move : piece_instance->generateMoves(current_board)) {
+            NodeData new_data{data.board_state};
+            new_data.board_state.previous_move = possible_move;
+            const BitBoard move_mask = (possible_move.old_position.get_bitboard_mask() | possible_move.new_position.get_bitboard_mask());
+
+            if (piece_instance->piece->colour == COL_WHITE) {
+                new_data.board_state.pieces_white ^= move_mask;
+            } else {
+                new_data.board_state.pieces_black ^= move_mask;
+            }
+
+            switch (piece_instance->piece->type) {
+                case PIECE_KING:
+                    new_data.board_state.pieces_kings ^= move_mask;
+                    break;
+                case PIECE_QUEEN:
+                    new_data.board_state.pieces_queens ^= move_mask;
+                    break;
+                case PIECE_ROOK:
+                    new_data.board_state.pieces_rooks ^= move_mask;
+                    break;
+                case PIECE_BISHOP:
+                    new_data.board_state.pieces_bishops ^= move_mask;
+                    break;
+                case PIECE_KNIGHT:
+                    new_data.board_state.pieces_knights ^= move_mask;
+                    break;
+                case PIECE_PAWN:
+                    new_data.board_state.pieces_pawns ^= move_mask;
+                    break;
+                default: break;
+            }
+
             if (new_data.initialise_node()) {
                 add_child(new_data);
             }
-            //todo: modify this to reflect the change
         }
     }
 }
--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,14 +1,29 @@
-#include "boardstate.h"
+#include "decisiontree.h"
 
 #include <iostream>
 #include <bitset>
 
-int main() {
-    BoardState initial_state;
-    initial_state.setup_default();
+#include "decisiontree.h"
 
-    initial_state.print();
-    std::cout << std::endl << initial_state.get_fen();
+int main() {
+    BoardState initial_board_state;
+    initial_board_state.setup_default();
+
+    DecisionTree tree;
+    tree.root = std::make_unique<DecisionTreeNode>(NodeData(initial_board_state));
+    tree.root->data.board_state.print();
+
+    std::cout << tree.root->children.size() << std::endl;
+
+    tree.root->generate_moves();
+
+    std::cout << tree.root->children.size() << std::endl;
+    for (int i = 0; i < tree.root->children.size(); i++) {
+        Move move = tree.root->children[i]->data.board_state.previous_move.value();
+        move.print();
+        std::cout << std::endl;
+        tree.root->children[i]->data.board_state.print();
+    }
 
     return 0;
 }
